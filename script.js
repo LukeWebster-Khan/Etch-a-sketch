4c8ef818a1895c91015d3038bcfcbfb8b99e4406
Luke <lukewebsterkhan@gmail.com>
2021-08-19 14:38:58 +0100

    added slider value function

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const buttons = document.querySelectorAll('buttons')
-const screen = document.querySelector('.screen')
+const buttons = document.querySelectorAll("buttons");
+const screen = document.querySelector(".screen");
 
-let pixel = '';
-let gridSize = 50;
+let pixel = "";
+let gridSize = 10;
 
 const drawGrid = (screenSize) => {
-    for (i = 0; i < screenSize ** 2; i++){
-    pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    pixel.style.backgroundColour = 'white';
-    screen.appendChild(pixel); }
+  for (i = 0; i < screenSize ** 2; i++) {
+    pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.style.backgroundColour = "white";
+    screen.appendChild(pixel);
+  }
 
-    screen.style.gridTemplateColumns = `repeat(${screenSize},auto)`;
-    screen.style.gridTemplateRows = `repeat(${screenSize},auto)`;
-}
+  screen.style.gridTemplateColumns = `repeat(${screenSize},auto)`;
+  screen.style.gridTemplateRows = `repeat(${screenSize},auto)`;
+};
+
+drawGrid(gridSize);
+
+// slider display
+let slider = document.getElementById("sizeSlider");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
